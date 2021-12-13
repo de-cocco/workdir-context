8281222aa04003b23966cf378ef140c95fb652cb
de-cocco <95973425+de-cocco@users.noreply.github.com>
2021-12-13 13:17:04 +0100

    Update workdir_context.py

--- workdir_context.py
+++ workdir_context.py
@@ -6,20 +6,20 @@ from contextlib import contextmanager
 @contextmanager
 def workdir(path):
     """
-    Takes a path and provides a context with that path as working directory
+    Takes a path and provides a context with that path as working directory.
     
     Args:
-        path (str): path the working directory should be changed to
+        path (str): path the working directory should be changed to.
     
     Yields:
         None
         
     Raises:
-        TypeError: If path is not string
+        TypeError: If path is not string.
     """
     
     if not isinstance(path, str):
-        raise TypeError('Path should be a string')
+        raise TypeError('Path should be a string.')
     
     try:
         prev_wd = os.getcwd()
